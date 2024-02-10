4397f4ddcd11cc7dfdda92894b6a0e1d17094426
6Mushahid9 <mushahid.study@gmail.com>
2024-02-10 23:44:29 +0530

    day 4 again

--- queries.sql
+++ queries.sql
@@ -3,7 +3,9 @@ show databases;
 use travelSystem;
 create table sign(username varchar(20), password varchar(20), question varchar(50), answer varchar(20));
 select * from sign;
-delete from sign where username = '';
+delete from sign where username = 'Talha bhai';
 create table userDetail(username varchar(20), id varchar(20), number varchar(15), gender varchar(6), country varchar(30), address varchar(30), phone varchar(10), email varchar(30));
 select * from userDetail;
+delete from userDetail where username = '';
 show tables;
+
